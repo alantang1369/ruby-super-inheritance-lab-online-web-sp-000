371085c21a7059188ff48c2f331b209f188ecae3
Alan Tang <giabao_tang@yahoo.com>
2020-10-14 05:22:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student 
   def hello 
-    Puts "Hey there! I'm so excited to learn stuff."
+    puts "Hey there! I'm so excited to learn stuff."
   end
   def raise_hand 
     puts "Pick me!"
